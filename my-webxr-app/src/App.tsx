0e1e8ebbe75fb8171ad5409099be6612e58587bc
Jesse Haug <jessterman.haug@gmail.com>
2024-01-24 23:50:20 -0600

    Now has controllers, there is a hovering button that should be interactable, not tested yet

--- my-webxr-app/src/App.tsx
+++ my-webxr-app/src/App.tsx
@@ -1,68 +1,67 @@
-import  { useEffect } from 'react';
-import * as THREE from 'three';
-import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
-function App() {
+import { useState } from 'react'
+import { Interactive, XR, Controllers, VRButton } from '@react-three/xr'
+import { Sky, Text } from '@react-three/drei'
+import '@react-three/fiber'
+import './styles.css'
+import { Canvas } from '@react-three/fiber'
 
-  useEffect(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.xr.enabled = true;
-    document.body.appendChild(VRButton.createButton(renderer));
+function Floor() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+      <planeGeometry args={[100, 100]} />
+      <meshStandardMaterial color="#72b2f4" />
+    </mesh>
+  )
+}
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    const controller1 = renderer.xr.getController(0);
-    const controller2 = renderer.xr.getController(1);
+function Box({ color, size, scale, children, ...rest }: any) {
+  return (
+    <mesh scale={scale} {...rest}>
+      <boxGeometry args={size} />
+      <meshPhongMaterial color={color} />
+      {children}
+    </mesh>
+  )
+}
 
-    
-    scene.add(controller2);
-    scene.add(controller1);
+function Button(props: any) {
+  const [hover, setHover] = useState(false)
+  const [color, setColor] = useState(0x123456)
 
-    scene.add(cube);
-
-    camera.position.z = 5;
-
-    const handleRender = () => {
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      renderer.render(scene, camera);
-    };
-
-    const handleResize = () => {
-      const newWidth = window.innerWidth;
-      const newHeight = window.innerHeight;
-
-      camera.aspect = newWidth / newHeight;
-      camera.updateProjectionMatrix();
-
-      renderer.setSize(newWidth, newHeight);
-    };
-
-    const animate = () => {
-      renderer.setAnimationLoop(() => {
-        handleRender();
-      });
-
-      window.addEventListener('resize', handleResize);
-
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    };
-
-    animate();
-
-    return () => {
-    };
-  }, []);
+  const onSelect = () => {
+    console.log("This button is interactable and has been clicked!")
+    setColor((Math.random() * 0xffffff) | 0)
+  }
 
   return (
-    <div>
-
-    </div>
-  );
+    <Interactive onSelect={onSelect} onHover={() => setHover(true)} onBlur={() => setHover(false)}>
+      <Box color={"#5fa2c2"} scale={hover ? [2.5, 2.5, 2.5] : [1, 1, 1]} size={[0.4, 0.1, 0.1]} {...props}>
+        <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
+          Hello Everyone!
+        </Text>
+      </Box>
+    </Interactive>
+  )
 }
-export default App;
+
+export default function App() {
+  return (
+    <>
+      <VRButton />
+      <Canvas>
+        <XR>
+          <Sky sunPosition={[0, 1, 0]} />
+          <Floor />
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Controllers />
+          <Button position={[0, 1.5, -1]} />
+        </XR>
+      </Canvas>
+    </>
+  )
+}
+
+// //@ts-ignore
+// const root = createRoot(document.getElementById('root'))
+// root.render(<App />)
