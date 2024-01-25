2564cdbb4548973ea0dffe127970e9608457df93
Long Quan Nguyen <imq026@usask.ca>
2024-01-24 21:45:42 -0600

    Url and local file input both work, add asertion in reader components

--- src/components/LocalReader.tsx
+++ src/components/LocalReader.tsx
@@ -6,9 +6,19 @@ function LocalReader({ setData }: { setData: (data: any[]) => void }) {
         const file = event.target.files?.[0];
         if (!file) return;
 
+        // Assert that the file is a CSV file
+        if (file.type !== 'text/csv') {
+            throw new Error('File must be a CSV file');
+        }
+
         Papa.parse(file, {
             header: true,
             complete: (results) => {
+                // Assert that the data is an array
+                if (!Array.isArray(results.data)) {
+                    throw new Error('Parsed data must be an array');
+                }
+
                 setData(results.data);
                 //console.table(results.data);
             },
--- src/components/UrlReader.tsx
+++ src/components/UrlReader.tsx
@@ -4,14 +4,19 @@ import Papa from 'papaparse';
 function UrlReader({ setData }: { setData: (data: any[]) => void }) {
     const handleUrlChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const url = event.target.value;
-        if (!url) return;
+        if (!url) throw new Error('URL cannot be empty');
 
         Papa.parse(url, {
             download: true,
             header: true,
             complete: (results) => {
+                // Assert that the data is an array
+                if (!Array.isArray(results.data)) {
+                    throw new Error('Parsed data must be an array');
+                }
+
                 setData(results.data);
-                console.table(results.data);
+                //console.table(results.data);
             },
         });
     };
