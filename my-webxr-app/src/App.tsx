817f1a13975368da8b441ed5dc70d962c919a98a
Jesse Haug <jessterman.haug@gmail.com>
2024-01-25 11:09:27 -0600

    Created a few more components to test everything

--- my-webxr-app/src/App.tsx
+++ my-webxr-app/src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { Interactive, XR, Controllers, VRButton } from '@react-three/xr'
 import { Sky, Text } from '@react-three/drei'
 import '@react-three/fiber'
 import './styles.css'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 
+
+//creating a floor, all it is is a plane with a color
 function Floor() {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]}>
@@ -14,6 +16,31 @@ function Floor() {
   )
 }
 
+//creating rotation boxes that accepts props to be used in the canvas
+//the useFrame hook is used to rotate the box
+//the Interactive component is used to make the box interactable
+
+function RotatingBox(props: JSX.IntrinsicElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+
+  return (
+    <Interactive onSelect={() => click(!clicked)} onHover={()=>hover(!hovered)} >
+      <mesh
+        {...props}
+        ref={ref}
+        scale={clicked ? .5 : .8}>
+        <boxGeometry args={[.5, .5, .5]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'blue'} />
+      </mesh>
+    </Interactive>
+  )
+}
+
+//creating a box that accepts props to be used in the canvas
+// can surround other things in this to creat things like text boxes/menus 
 function Box({ color, size, scale, children, ...rest }: any) {
   return (
     <mesh scale={scale} {...rest}>
@@ -24,10 +51,16 @@ function Box({ color, size, scale, children, ...rest }: any) {
   )
 }
 
+//creating a button that accepts props to be used in the canvas
+//the Interactive component is used to make the box interactable
+//the useState hook is used to change the color of the box
+//the useState hook is used to change the scale of the box
+// taken from the Webxr example 
 function Button(props: any) {
   const [hover, setHover] = useState(false)
   const [color, setColor] = useState(0x123456)
 
+  
   const onSelect = () => {
     console.log("This button is interactable and has been clicked!")
     setColor((Math.random() * 0xffffff) | 0)
@@ -35,7 +68,7 @@ function Button(props: any) {
 
   return (
     <Interactive onSelect={onSelect} onHover={() => setHover(true)} onBlur={() => setHover(false)}>
-      <Box color={"#5fa2c2"} scale={hover ? [2.5, 2.5, 2.5] : [1, 1, 1]} size={[0.4, 0.1, 0.1]} {...props}>
+      <Box color={color} scale={hover ? [2.5, 2.5, 2.5] : [1, 1, 1]} size={[0.4, 0.1, 0.1]} {...props}>
         <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
           Hello Everyone!
         </Text>
@@ -56,6 +89,9 @@ export default function App() {
           <pointLight position={[10, 10, 10]} />
           <Controllers />
           <Button position={[0, 1.5, -1]} />
+          <RotatingBox position={[0.8, 1.5, -1]} />
+          <RotatingBox position={[-0.8, 1.5, -1]} />
+
         </XR>
       </Canvas>
     </>
