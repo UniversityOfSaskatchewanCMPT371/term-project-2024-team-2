8db077f14ffa1d370bbae31299b7fcce409e899b
Long Quan Nguyen <imq026@usask.ca>
2024-01-31 17:15:31 -0600

    Add a test file to check log funtionality, this also implies jest for testing is working fine

--- my-webxr-app/tests/log-config.test.tsx
+++ my-webxr-app/tests/log-config.test.tsx
@@ -1,22 +1,34 @@
-import * as log4js from 'log4js';
+import * as log4js from "log4js";
+import * as fs from 'fs';
+import { logConfig } from '../src/log-config';
 
-jest.mock('log4js', () => ({
-    configure: jest.fn(),
-}));
+describe('Logger Configuration', () => {
+    let logger;
 
-describe('log4js configuration', () => {
-    it('should configure log4js correctly', () => {
-        require('../src/log-config');
+    // Apply the config from the config file
+    beforeAll(() => {
+        log4js.configure(logConfig);
+        logger = log4js.getLogger();
+    });
 
-        expect(log4js.configure).toHaveBeenCalledWith({
-            appenders: {
-                everything: { type: 'file', filename: '../../log/info-file-logger.log' },
-                errors: { type: 'file', filename: '../../log/error-file-logger.log' }
-            },
-            categories: {
-                default: { appenders: [ 'everything' ], level: 'info' },
-                error: { appenders: [ 'everything', 'errors' ], level: 'error' }
-            }
+    it('should log message correctly', (done) => {
+        logger.info('Test info message');
+        logger.warn('Test warn message');
+        logger.error('Test error message');
+        logger.fatal('Test fatal message');
+
+        log4js.shutdown(() => {
+            const infoLogContent = fs.readFileSync('../logs/info-file-logger.log', 'utf8');
+            expect(infoLogContent).toContain('Test info message');
+            expect(infoLogContent).toContain('Test warn message');
+            expect(infoLogContent).toContain('Test error message');
+            expect(infoLogContent).toContain('Test fatal message');
+
+            const errorLogContent = fs.readFileSync('../logs/error-file-logger.log', 'utf8');
+            expect(errorLogContent).toContain('Test error message');
+            expect(errorLogContent).toContain('Test fatal message');
+
+            done();
         });
     });
 });
