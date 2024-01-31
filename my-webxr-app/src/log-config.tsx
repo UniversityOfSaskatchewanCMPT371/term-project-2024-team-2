07d35a5c5d9503fe640dbead550652c629650efa
Long Quan Nguyen <imq026@usask.ca>
2024-01-31 17:14:00 -0600

    Remove uncessary error logger mode

--- my-webxr-app/src/log-config.tsx
+++ my-webxr-app/src/log-config.tsx
@@ -1,12 +1,9 @@
-import * as log4js from "log4js";
-
-log4js.configure({
+export const logConfig = {
     appenders: {
-        everything: { type: 'file', filename: '../../log/info-file-logger.log' },
-        errors: { type: 'file', filename: '../../log/error-file-logger.log' }
+        everything: { type: 'file', filename: '../logs/info-file-logger.log' },
+        errors: { type: 'file', filename: '../logs/error-file-logger.log' }
     },
     categories: {
-        default: { appenders: [ 'everything' ], level: 'info' },
-        error: { appenders: [ 'everything', 'errors' ], level: 'error' }
+        default: { appenders: [ 'everything' , 'errors' ], level: 'info' }
     }
-});
+};
