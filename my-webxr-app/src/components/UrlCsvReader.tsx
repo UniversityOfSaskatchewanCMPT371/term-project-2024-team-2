9ea99fd7f1e2259fd1b6e862b9160266a2b87355
Long Quan Nguyen <imq026@usask.ca>
2024-02-09 12:12:47 -0600

    #11 split URLCsvReader to smaller function to make it testable, add some unit test

--- my-webxr-app/src/components/UrlCsvReader.tsx
+++ my-webxr-app/src/components/UrlCsvReader.tsx
@@ -2,80 +2,96 @@ import React, { useState } from 'react';
 import Papa from 'papaparse';
 import { openDB } from 'idb';
 
+/**
+ * Validates a URL to ensure it points to a CSV file.
+ *
+ * @param {string} url - The URL to validate.
+ * @throws {Error} Will throw an error if the URL does not end with '.csv' or is empty.
+ */
+export const validateUrl = (url: string) => {
+    if (!url.endsWith('.csv')) {
+        throw new Error('URL must point to a CSV file or not empty');
+    }
+};
+
+/**
+ * Validates the existence of a database and a store within that database.
+ *
+ * @param {string} dbName - The name of the database to validate.
+ * @param {string} storeName - The name of the store to validate.
+ * @throws {Error} Will throw an error if the database does not exist, or if the store does not exist within the
+ * database.
+ */
+export const validateDbAndStore = async (dbName: string, storeName: string) => {
+    const db = await openDB(dbName, 1);
+    const dbExists = db !== undefined;
+    if (!dbExists) {
+        throw new Error(`Database "${dbName}" does not exist`);
+    }
+
+    const storeExists = db.objectStoreNames.contains(storeName);
+    if (!storeExists) {
+        throw new Error(`Store "${storeName}" does not exist in database "${dbName}"`);
+    }
+};
+
+/**
+ * Handles the parsed data from a CSV file and stores it in a database.
+ *
+ * @param {any} results - The results object returned by Papa.parse. The object data should be an array.
+ * @param {string} dbName - The name of the database where the data should be stored.
+ * @param {string} storeName - The name of the store within the database where the data should be stored.
+ * @throws {Error} Will throw an error if the 'data' property of the results object is not an array.
+ */
+export const handleParsedData = async (results: any, dbName: string, storeName: string) => {
+    if (!Array.isArray(results.data)) {
+        throw new Error('Parsed data must be an array');
+    }
+
+    const db = await openDB(dbName, 1);
+    const tx = db.transaction(storeName, 'readwrite');
+    const store = tx.objectStore(storeName);
+    await store.clear(); 
+    for (let i = 0; i < results.data.length; i++) {
+        const item = results.data[i];
+        await store.put(item, i);
+    }
+    await tx.done;
+};
+
 interface UrlCsvReaderProps {
     dbName: string;
     storeName: string;
 }
 
 /**
- * UrlCsvReader is a React component that provides an input field and a button for the user to enter a URL to a CSV file.
- * When the button is clicked, it uses the Papa Parse library to download and parse the CSV file.
- * The parsed data is then stored in an IndexedDB database.
+ * A React component that reads data from a CSV file at a given URL and stores it in a specified database and store.
  *
- * Props:
- * - dbName: The name of the IndexedDB database where the parsed data will be stored.
- * - storeName: The name of the object store within the database where the parsed data will be stored.
+ * @param {object} props - The properties passed to the component.
+ * @param {string} props.dbName - The name of the database where the data should be stored.
+ * @param {string} props.storeName - The name of the store within the database where the data should be stored.
  *
- * The component throws an error if:
- * - The URL does not point to a CSV file, this cover the empty case.
- * - The specified database does not exist.
- * - The specified store does not exist in the database.
- * - The parsed data is not an array.
- *
- * The component also provides a feedback popup that is displayed when the CSV data is successfully read and stored.
+ * @returns {JSX.Element} A form with an input field for the CSV URL and a button to load the CSV data. After successful
+ * loading, a success message is displayed.
  */
 export function UrlCsvReader({ dbName, storeName }: UrlCsvReaderProps) {
-    // A feedback pop up to check if csv read in is sucesefully
     const [showPopup, setShowPopup] = useState(false);
-    
     const [url, setUrl] = useState(''); 
+
     const handleUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setUrl(event.target.value); 
     };
 
     const handleButtonClick = async () => {
-        // Check if the URL ends with '.csv'
-        if (!url.endsWith('.csv')) {
-            throw new Error('URL must point to a CSV file');
-        }
+        validateUrl(url);
+        await validateDbAndStore(dbName, storeName);
 
-        // Assert that the database is intialized
-        const db = await openDB(dbName, 1);
-        const dbExists = db !== undefined;
-        if (!dbExists) {
-            throw new Error(`Database "${dbName}" does not exist`);
-        }
-
-        // Assert that the store exists in the database
-        const storeExists = db.objectStoreNames.contains(storeName);
-        if (!storeExists) {
-            throw new Error(`Store "${storeName}" does not exist in database "${dbName}"`);
-        }
-
-        // Parsing the csv data
         Papa.parse(url, {
             download: true,
             header: true,
-            dynamicTyping: true, // Auto convert values to numbers if possible
+            dynamicTyping: true, // Convert data to number type if applicable
             complete: async (results) => {
-
-                // Assert that the parsed data is an array, parsing was successful
-                if (!Array.isArray(results.data)) {
-                    throw new Error('Parsed data must be an array');
-                }
-
-                // Put the parsed data into the database store, overwriting.
-                const db = await openDB(dbName, 1);
-                const tx = db.transaction(storeName, 'readwrite');
-                const store = tx.objectStore(storeName);
-                await store.clear(); // Empty the store before writing new data
-                for (let i = 0; i < results.data.length; i++) {
-                    const item = results.data[i];
-                    await store.put(item, i);
-                }
-                await tx.done;
-
-                // Show the feedback popup
+                await handleParsedData(results, dbName, storeName);
                 setShowPopup(true);
             },
         });
