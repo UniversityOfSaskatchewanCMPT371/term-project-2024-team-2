68494c080f7488448690d80faf6e84bcbb0ce6eb
Joda <joda@Joes-Air.gateway.sasktel.net>
2024-02-13 18:29:10 -0600

    Add moduleDirectories configuration to jest.config.js
    This modification  allows Jest to look for modules not only in the node_modules directory but also in the src directory. It addresses how Jest handles module resolution and the import of Drei components

--- jest.config.js
+++ jest.config.js
@@ -4,3 +4,12 @@ const jestConfig = {
 };
 
 export default jestConfig;
+
+//This tells Jest to look for modules not only in the node_modules directory but also in the src directory
+module.exports = {
+    // other configurations
+    moduleDirectories: ['node_modules', 'src'],
+};
+
+
+
