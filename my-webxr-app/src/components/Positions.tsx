eecf4b80590a5c6a3b0c46f15621b3a0eeb28dda
Jesster2829 <jessterman.haug@gmail.com>
2024-02-29 11:13:17 -0600

    Asserts not working

--- my-webxr-app/src/components/Positions.tsx
+++ my-webxr-app/src/components/Positions.tsx
@@ -1,6 +1,5 @@
 import {Vector3} from "three";
-
-
+import * as assert from 'assert'
 /**
  * Creates a Vector for the points position relative to the size of the Axis and the Data given to the point ( would eventually want to create
  * a axis Type to pass in instead of all these variables )
@@ -12,18 +11,17 @@ import {Vector3} from "three";
  * @return {Vector3} - this is the final position of the point.
  */
 export function createPosition(data: Array<number>, AxisStartPoints:Array<number>, length:number, scale:number, max:number) {
-    if (data.length !== 3 || AxisStartPoints.length !== 3 || length === undefined || length < 0) {
-        throw new Error("Error in Positions.tsx: Parameters do not meet requirements. data must be an array of 3 numbers," +
-            "AxisStartPoints must have 3 entries, one for each axis, in the format [x,y,z], and AxisEndpoints must be " +
-            "greater than or equal to 0 ");
-    }
-    if(data[0] > max || data[1] > max || data[2] > max){
-        throw new Error("Error in Positions.tsx: Trying to map Data that does not exist within the domain " +
-            "of the graph, please check ")
-    }
-    const x_position = ((data[0]/max) * (scale*length)/2 ) + AxisStartPoints[0]
-    const y_position = ((data[1]/max) * (scale*length)/2 ) + AxisStartPoints[1]
-    const z_position = ((data[2]/max) * (scale*length)/2 ) + AxisStartPoints[2]
+    // assert.equal(data.length==3,true,"Error in Positions.tsx, should be 3 entries of data to be mapped, [x,y,z]");
+    // assert.equal(AxisStartPoints.length==3,true,"Error in Positions.tsx, should be 3 start points, [x,y,z]");
+    // assert.equal(length!==undefined && length > 0, true, "Error in Position.tsx, length of Axis should never be 0 or less ");
+    // assert.equal(scale!==undefined && scale >0, true,"Error in Position.tsx, Scale should never be 0 or less");
+    // assert.equal(data[0] > max || data[1] > max || data[2] > max , false, "Error in Positions.tsx: Trying to map Data that does not exist within the domain " +
+    //     "of the graph, please check");
+    assert.equal(true,true,"fail")
+
+    const x_position = ((data[0]/max) * (scale*length)/2 ) + AxisStartPoints[0];
+    const y_position = ((data[1]/max) * (scale*length)/2 ) + AxisStartPoints[1];
+    const z_position = ((data[2]/max) * (scale*length)/2 ) + AxisStartPoints[2];
 
     return new Vector3(x_position,y_position,z_position);
 }
