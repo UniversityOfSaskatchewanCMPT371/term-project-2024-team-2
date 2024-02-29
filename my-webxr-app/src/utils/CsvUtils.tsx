8eeac7adcbd4fce825d9a244c52d730894aa1cc1
Long Quan Nguyen <imq026@usask.ca>
2024-02-29 15:46:09 -0600

    Fix linting part 2

--- my-webxr-app/src/utils/CsvUtils.tsx
+++ my-webxr-app/src/utils/CsvUtils.tsx
@@ -8,7 +8,8 @@ import assert from './Assert';
  *
  * @param {string} dbName - The name of the database to validate.
  * @param {string} storeName - The name of the store to validate.
- * @throws {Error} Will throw an error if the database does not exist, or if the store does not exist within the
+ * @throws {Error} Will throw an error if the database does not exist, or if the store does not
+ * exist within the
  * database.
  */
 export const validateDbAndStore = async (dbName: string, storeName: string) => {
@@ -24,10 +25,16 @@ export const validateDbAndStore = async (dbName: string, storeName: string) => {
  *
  * @param {Array<Array<string | number | null>>} items - The parsed CSV data to be stored.
  * @param {string} dbName - The name of the database where the data should be stored.
- * @param {string} storeName - The name of the store within the database where the data should be stored.
+ * @param {string} storeName - The name of the store within the database where the data should be
+ * stored.
  * @param {number} start - The starting index for the data to be stored.
  */
-export const handleParsedData = async (items: Array<Array<string | number | null>>, dbName: string, storeName: string, start: number) => {
+export const handleParsedData = async (
+  items: Array<Array<string | number | null>>,
+  dbName: string,
+  storeName: string,
+  start: number,
+) => {
   const db = await openDB(dbName, 1);
   const tx = db.transaction(storeName, 'readwrite');
   const store = tx.objectStore(storeName);
@@ -44,10 +51,17 @@ export const handleParsedData = async (items: Array<Array<string | number | null
  *
  * @param {File} file - The local CSV file to parse.
  * @param {string} dbName - The name of the database where the data should be stored.
- * @param {string} storeName - The name of the store within the database where the data should be stored.
- * @param {React.Dispatch<React.SetStateAction<string | null>>} setMessage - The function to call to set the message.
+ * @param {string} storeName - The name of the store within the database where the data should be
+ * stored.
+ * @param {React.Dispatch<React.SetStateAction<string | null>>} setMessage - The function to call to
+ * set the message.
  */
-export async function parseAndHandleLocalCsv(file: File, dbName: string, storeName: string, setMessage: React.Dispatch<React.SetStateAction<string | null>>) {
+export async function parseAndHandleLocalCsv(
+  file: File,
+  dbName: string,
+  storeName: string,
+  setMessage: React.Dispatch<React.SetStateAction<string | null>>,
+) {
   let i = 0;
   let batch :Array<Array<string | number | null>> = [];
   const batchSize = 1000; // # of rows per batch
@@ -75,10 +89,17 @@ export async function parseAndHandleLocalCsv(file: File, dbName: string, storeNa
  *
  * @param {string} url - The URL of the CSV file to parse.
  * @param {string} dbName - The name of the database where the data should be stored.
- * @param {string} storeName - The name of the store within the database where the data should be stored.
- * @param {React.Dispatch<React.SetStateAction<string | null>>} setMessage - The function to call to set the message.
+ * @param {string} storeName - The name of the store within the database where the data should be
+ * stored.
+ * @param {React.Dispatch<React.SetStateAction<string | null>>} setMessage - The function to call to
+ * set the message.
  */
-export async function parseAndHandleUrlCsv(url: string, dbName: string, storeName: string, setMessage: React.Dispatch<React.SetStateAction<string | null>>) {
+export async function parseAndHandleUrlCsv(
+  url: string,
+  dbName: string,
+  storeName: string,
+  setMessage: React.Dispatch<React.SetStateAction<string | null>>,
+) {
   let i = 0;
   let batch :Array<Array<string | number | null>> = [];
   const batchSize = 1000; // # of rows per batch
