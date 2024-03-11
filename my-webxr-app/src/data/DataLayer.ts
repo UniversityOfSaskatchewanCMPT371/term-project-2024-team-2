dcd029cb4728cd876a1b179587e2a81f355aee39
Subzeero <76539691+Subzeero@users.noreply.github.com>
2024-03-11 00:18:24 -0600

    #163: Singleton prototype
    * Create a prototype of implementing the DAL as a singleton.

--- my-webxr-app/src/data/DataLayer.ts
+++ my-webxr-app/src/data/DataLayer.ts
@@ -1,4 +1,23 @@
-import {DataAbstractor} from './DataAbstractor';
+import { DataAbstractor } from './DataAbstractor';
 
 export default class DataLayer implements DataAbstractor {
+  private static instance: DataLayer;
+
+  private myField: boolean;
+
+  private constructor() {
+    this.myField = true;
+  }
+
+  public static getInstance(): DataLayer {
+    if (!DataLayer.instance) {
+      DataLayer.instance = new DataLayer();
+    }
+    return DataLayer.instance;
+  }
+
+  async getAvailableFields() {
+    this.myField = false;
+    return Promise.resolve(true);
+  }
 }
