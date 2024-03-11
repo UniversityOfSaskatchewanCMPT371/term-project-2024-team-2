2a0b940e0b1294bd438ee3b072e89abd72507361
Subzeero <76539691+Subzeero@users.noreply.github.com>
2024-03-10 22:38:03 -0600

    #163: Update Promise types
    * Add types to the interface methods returning Promises.

--- my-webxr-app/src/data/DataAbstractor.ts
+++ my-webxr-app/src/data/DataAbstractor.ts
@@ -2,8 +2,8 @@ import DataPoint from '../repository/DataPoint';
 
 export interface DataAbstractor {
   getAvailableFields: () => Promise<Array<DataPoint>>;
-  storeCSV: (batchItems: Array<Array<string | number | null>>) => Promise;
-  storePCA: (batchItems: Array<Array<string | number | null>>) => Promise;
-  selectAxes: (columnXName: string, columnYName: string, columnZName: string) => Promise;
-  selectPCA: (columnXName: string, columnYName: string, columnZName: string) => Promise;
+  storeCSV: (batchItems: Array<Array<string | number | null>>) => Promise<boolean>;
+  storePCA: (batchItems: Array<Array<string | number | null>>) => Promise<boolean>;
+  selectAxes: (columnXName: string, columnYName: string, columnZName: string) => Promise<boolean>;
+  selectPCA: (columnXName: string, columnYName: string, columnZName: string) => Promise<boolean>;
 }
