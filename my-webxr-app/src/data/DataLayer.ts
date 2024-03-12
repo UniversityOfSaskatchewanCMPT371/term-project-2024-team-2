aec8e10817494ac274ec6b3902a92fa95806e984
Subzeero <76539691+Subzeero@users.noreply.github.com>
2024-03-12 01:14:39 -0600

    #163: Implement transpose and stats calc
    * From group pairing with Jesse and Quinn, implemented transposeData() and calculateStatistics().
    * Going to discuss how to deal with processing partial columns at team standup.

--- my-webxr-app/src/data/DataLayer.ts
+++ my-webxr-app/src/data/DataLayer.ts
@@ -7,7 +7,7 @@ import DataPoint from '../repository/DataPoint';
  * The Data Layer provides a set of methods for working with CSV and PCA data.
  */
 export default class DataLayer implements DataAbstractor {
-  // @ts-expect-error temp disable
+  // @ts-expect-error temp ignore
   private repository: Repository;
 
   /**
@@ -17,6 +17,52 @@ export default class DataLayer implements DataAbstractor {
    */
   private constructor(dbName?: string) {
     this.repository = new DbRepository(dbName ?? 'DAL_DB');
+  }
+
+  /**
+   * Transport a stream of batched data to be referenced column-wise instead of row-wise.
+   * @param batchItems A 2D array of CSV data that is referenced by row.
+   * @private
+   */
+  private static transposeData(batchItems: Array<Array<string | number | null>>) {
+    const rows = batchItems.length;
+    const cols = batchItems[0].length;
+    const grid: Array<Array<string | number | null>> = [];
+    for (let j = 0; j < cols; j += 1) {
+      grid[j] = Array(rows);
+    }
+    for (let i = 0; i < rows; i += 1) {
+      for (let j = 0; j < cols; j += 1) {
+        grid[j][i] = batchItems[i][j];
+      }
+    }
+    return grid;
+  }
+
+  /**
+   * Calculate the statistics for a set of data. Items that are not a number are treated as 0.
+   * @param batchItems transposed data referenced column-wise.
+   * @private
+   */
+  private static calculateStatistics(batchItems: Array<Array<string | number | null>>) {
+    const statsArray: Array<NonNullable<unknown>> = [];
+    batchItems.forEach((column) => {
+      const numberedItems = column.map((item) => {
+        if (typeof item === 'number') {
+          return item;
+        }
+        return 0;
+      });
+      const count = numberedItems.length - 1;
+      const sampleDev = numberedItems.reduce((runningTotal, x) => runningTotal + x, 0);
+      const sampleStdDev = numberedItems.reduce((runningTotal, x) => runningTotal + x ** 2, 0);
+      const mean = sampleDev / count;
+      const stdDev = Math.sqrt((sampleStdDev / count) - (mean ** 2));
+      statsArray.push({
+        columnName: column[0], sampleDev, sampleStdDev, mean, stdDev,
+      });
+    });
+    return statsArray;
   }
 
   /**
@@ -33,7 +79,14 @@ export default class DataLayer implements DataAbstractor {
    */
   // temp disable
   // eslint-disable-next-line class-methods-use-this
-  async storeCSV() {
+  async storeCSV(batchItems: Array<Array<string | number | null>>) {
+    const transposedData = DataLayer.transposeData(batchItems);
+    // @ts-expect-error temp disable
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const itemsStats = DataLayer.calculateStatistics(transposedData);
+
+    // TODO: Find a way to deal with partial columns
+    // this.repository.addColumn();
     return Promise.resolve(true);
   }
 
