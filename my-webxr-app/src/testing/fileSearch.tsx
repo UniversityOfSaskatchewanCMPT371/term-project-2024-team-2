85b168731ab2b715e89b65154d68281ddd21b663
Mitchell Wagner <pim128@usask.ca>
2024-03-13 13:27:17 -0600

    #137 removed unnecesary code

--- my-webxr-app/src/testing/fileSearch.tsx
+++ my-webxr-app/src/testing/fileSearch.tsx
@@ -1,27 +1,3 @@
-// import * as fs from 'fs';
-// import * as readline from 'readline';
-
-// const filePath = 'src/testing/testLog.txt';
-
-// export async function containsText(text: string): Promise<boolean> {
-//     const reader = readline.createInterface({
-//         input: fs.createReadStream(filePath),
-//         output: process.stdout,
-//         terminal: false,
-//       });
-//     return new Promise<boolean>((resolve) => {
-//         reader.on('line', (line: string) => {
-//             if (line.toLowerCase().includes(text.toLowerCase())) {
-//                 resolve(true);
-//             }
-//         });
-//         reader.on('close', () => {
-//             resolve(false);
-//         });
-//     });
-// }
-
-
 import * as fs from 'fs';
 import * as readline from 'readline';
 
