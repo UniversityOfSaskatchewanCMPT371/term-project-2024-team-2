8acbb3392c996d3c071cb28145a60ac4ceae6e90
trangnguyen3010 <66387098+trangnguyen3010@users.noreply.github.com>
2024-03-21 14:52:12 -0600

    #193: feel free to remove this commit - change the problematic import statements from ESM to commonjs style

--- my-webxr-app/src/testing/TextInFileSearch.tsx
+++ my-webxr-app/src/testing/TextInFileSearch.tsx
@@ -1,5 +1,11 @@
-import * as fs from 'fs';
-import * as readline from 'readline';
+// import * as fs from 'fs';
+// import * as readline from 'readline';
+
+// TODO: I don't know why this works
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const readline = require('readline');
 
 export default async function ContainText(text: string): Promise<boolean> {
   const filePath = 'src/testing/testLog.txt';
