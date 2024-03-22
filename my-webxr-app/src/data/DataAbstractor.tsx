d864e671235521600943dedcdc13d413a0ca341d
Long Quan Nguyen <imq026@usask.ca>
2024-03-22 00:40:37 -0600

    #163  Update calculateStatistics and implement gatAvailableFields

--- my-webxr-app/src/data/DataAbstractor.tsx
+++ my-webxr-app/src/data/DataAbstractor.tsx
@@ -1,11 +1,9 @@
-import DataPoint from '../repository/DataPoint';
-
 /**
  * The DataAbstractor interface defines a set of common operations available to the Data
  * Abstraction Layer.
  */
 export default interface DataAbstractor {
-  getAvailableFields: () => Promise<Array<DataPoint>>;
+  getAvailableFields: () => Promise<string[]>;
   storeCSV: (batchItems: Array<Array<string | number | null>>) => Promise<boolean>;
   storePCA: (batchItems: Array<Array<string | number | null>>) => Promise<boolean>;
   selectAxes: (columnXName: string, columnYName: string, columnZName: string) => Promise<boolean>;
--- my-webxr-app/src/data/DataLayer.tsx
+++ my-webxr-app/src/data/DataLayer.tsx
@@ -1,7 +1,6 @@
 import DataAbstractor from './DataAbstractor';
 import { Repository } from '../repository/Repository';
 import DbRepository from '../repository/DbRepository';
-import DataPoint from '../repository/DataPoint';
 import Column, {
   ColumnType, DataColumn, DataRow, StatsColumn,
 } from '../repository/Column';
@@ -22,6 +21,7 @@ export default class DataLayer implements DataAbstractor {
 
   /**
    * Transport a stream of batched data to be referenced column-wise instead of row-wise.
+   *
    * @param batchItems A 2D array of CSV data that is referenced by row.
    * @returns batchItems transposed to be referenced by column instead of by row.
    * @protected
@@ -30,58 +30,73 @@ export default class DataLayer implements DataAbstractor {
     const rows = batchItems.length;
     const cols = batchItems[0]?.length ?? 0;
     const transposedItems: BatchedDataStream = [];
-
     for (let j = 0; j < cols; j += 1) {
       transposedItems[j] = Array(rows);
     }
-
     for (let i = 0; i < rows; i += 1) {
       for (let j = 0; j < cols; j += 1) {
         transposedItems[j][i] = batchItems[i][j];
       }
     }
-
     return transposedItems;
   }
 
   /**
-   * Calculate the statistics for a set of data. Items that are not a number are treated as 0.
-   * @param batchItems transposed data referenced column-wise.
-   * @returns an array of StatsColumn for each column.
-   * @protected
-   */
-  protected static calculateStatistics(batchItems: BatchedDataStream) {
-    const statsArray: Array<StatsColumn> = [];
+ * Calculate the statistics for a set of data. Items that are not a number are treated as 0.
+ * This method retrieves all column names from the repository, then for each column, it calculates
+ * the count, sum, sum of squares, mean, and standard deviation. It then creates a new StatsColumn
+ * object and adds it to the repository.
+ *
+ * @returns {Promise<boolean>} A promise that resolves to true if the operation was successful,
+ * false otherwise.
+ */
+  async calculateStatistics(): Promise<boolean> {
+    try {
+      const rawColumnNames = await this.repository.getAllColumnNames();
 
-    batchItems.forEach((column) => {
-      // Convert non-number types into 0 for the stats calculations.
-      const numberedItems = column.slice(1).map((item) => ((typeof item === 'number') ? item : 0));
+      // eslint-disable-next-line no-restricted-syntax
+      for (const columnName of rawColumnNames) {
+        // Retrieve the column data from the repository
+        // eslint-disable-next-line no-await-in-loop
+        const column = await this.repository.getDataColumn(columnName, ColumnType.RAW);
 
-      const count = numberedItems.length;
-      const sum = numberedItems.reduce((runningTotal, x) => runningTotal + x, 0);
-      const sumOfSquares = numberedItems.reduce((runningTotal, x) => runningTotal + x ** 2, 0);
-      const mean = sum / ((count !== 0) ? count : 1);
-      const stdDev = (
-        (count >= 2)
-          ? (Math.sqrt(((sumOfSquares / ((count !== 0) ? count : 1)) - (mean ** 2))))
-          : 0
-      );
+        const numberedItems = column.values.map((item) => ((typeof item === 'number') ? item : 0));
 
-      statsArray.push({
-        columnName: column[0], sum, sumOfSquares, mean, stdDev,
-      });
-    });
+        const count = numberedItems.length;
+        const sum = numberedItems.reduce((runningTotal, x) => runningTotal + x, 0);
+        const sumOfSquares = numberedItems.reduce((runningTotal, x) => runningTotal + x ** 2, 0);
+        const mean = sum / ((count !== 0) ? count : 1);
+        const stdDev = (
+          (count >= 2)
+            ? (Math.sqrt(((sumOfSquares / ((count !== 0) ? count : 1)) - (mean ** 2))))
+            : 0
+        );
 
-    return statsArray;
+        const statsColumn = new Column<StatsColumn>(columnName, {
+          count,
+          sum,
+          sumOfSquares,
+          mean,
+          stdDev,
+        });
+
+        // eslint-disable-next-line no-await-in-loop
+        await this.repository.addColumn(statsColumn, ColumnType.STATS);
+      }
+      return true;
+    } catch (error) {
+      return false;
+    }
   }
 
   /**
-   * WIP
-   */
-  // temp disable
-  // eslint-disable-next-line class-methods-use-this
-  async getAvailableFields() {
-    return Promise.resolve([new DataPoint(false, 1, 2, 3)]);
+ * Retrieve the available fields (column headers) from the data repository.
+ * This method calls the `getAllColumnNames` method of the `Repository` instance.
+ *
+ * @returns {Promise<string[]>} A promise that resolves to an array of column names.
+ */
+  async getAvailableFields(): Promise<string[]> {
+    return this.repository.getAllColumnNames();
   }
 
   /**
@@ -109,7 +124,7 @@ export default class DataLayer implements DataAbstractor {
   }
 
   /**
-   * WIP
+   * need to clauclate satandreized data for each column
    */
   // temp disable
   // eslint-disable-next-line class-methods-use-this
@@ -119,6 +134,7 @@ export default class DataLayer implements DataAbstractor {
 
   /**
    * WIP
+   * slect a clumn for a axis return data poiunt of the whole volumn
    */
   // temp disable
   // eslint-disable-next-line class-methods-use-this
@@ -128,6 +144,7 @@ export default class DataLayer implements DataAbstractor {
 
   /**
    * WIP
+   * slect a pca cplumn fo an axis
    */
   // temp disable
   // eslint-disable-next-line class-methods-use-this
