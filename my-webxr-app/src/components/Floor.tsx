e812d246837897d5a9ee65b2cac8bb3cc8b41e7c
Aesha7 <paesha27@gmail.com>
2024-03-22 21:33:27 -0600

    #204: redo dark UI + place billboard behind axis

--- my-webxr-app/src/components/Floor.tsx
+++ my-webxr-app/src/components/Floor.tsx
@@ -3,7 +3,7 @@ export default function Floor() {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]}>
       <planeGeometry args={[100, 100]} />
-      <meshStandardMaterial color="#72b2f4" />
+      <meshStandardMaterial color="gray" />
     </mesh>
   );
 }
