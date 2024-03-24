1ea046dd15aeceb82ec3764cddb50ddc3d022feb
trangnguyen3010 <66387098+trangnguyen3010@users.noreply.github.com>
2024-03-24 11:03:55 -0600

    #117: add docs to mock worker

--- my-webxr-app/tests/MockServer.tsx
+++ my-webxr-app/tests/MockServer.tsx
@@ -11,6 +11,12 @@ const data = await readFile('./src/assets/sans-serif.normal.100.woff');
 const fontFile = Uint8Array.from(data).buffer;
 
 // 2. Describe network behavior with request handlers.
+// Drei Text component doesn't contain the font file,
+// it is fetched either through a FetchAPI or a XMLHttpRequest (uint8 arraybuffer response).
+// (if we specify the 'font' attribute (font URL) in the Text component,
+// the font will be fetched through a XMLHttpRequest,
+// otherwise all fallback fonts will be fetched through FetchAPI)
+// Thus, to test the Text component, we have to mock the font file request.
 const worker = setupServer(
   http.get(
     'https://cdn.jsdelivr.net/gh/lojjic/unicode-font-resolver@v1.0.1/packages/data/*',
