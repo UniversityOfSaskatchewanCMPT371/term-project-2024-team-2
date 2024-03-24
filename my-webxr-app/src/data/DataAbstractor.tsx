ccbf5c1cc0d95f4792642f9b53892e647b95a5c4
Long Quan Nguyen <imq026@usask.ca>
2024-03-24 01:40:10 -0600

    #163  Revert last commit

--- my-webxr-app/src/data/DataAbstractor.tsx
+++ my-webxr-app/src/data/DataAbstractor.tsx
@@ -1,13 +1,10 @@
-// eslint-disable-next-line import/no-cycle
-import { BatchedDataStream } from './DataLayer';
-
 /**
  * The DataAbstractor interface defines a set of common operations available to the Data
  * Abstraction Layer.
  */
 export default interface DataAbstractor {
   getAvailableFields: () => Promise<string[]>;
-  storeCSV: (batchItems: BatchedDataStream) => Promise<boolean>;
+  storeCSV: (batchItems: Array<Array<string> | Array<number>>) => Promise<boolean>;
   storePCA: () => Promise<boolean>;
   selectAxes: (columnXName: string, columnYName: string, columnZName: string) => Promise<boolean>;
   selectPCA: (columnXName: string, columnYName: string, columnZName: string) => Promise<boolean>;
