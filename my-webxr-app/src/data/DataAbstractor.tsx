422dd79db815056b5c5d7ca8b671444f3a76b1cf
Long Quan Nguyen <imq026@usask.ca>
2024-03-24 01:38:10 -0600

    #163  Update type specification for dataAbstractor IF, per Trang's suggestion

--- my-webxr-app/src/data/DataAbstractor.tsx
+++ my-webxr-app/src/data/DataAbstractor.tsx
@@ -1,10 +1,13 @@
+// eslint-disable-next-line import/no-cycle
+import { BatchedDataStream } from './DataLayer';
+
 /**
  * The DataAbstractor interface defines a set of common operations available to the Data
  * Abstraction Layer.
  */
 export default interface DataAbstractor {
   getAvailableFields: () => Promise<string[]>;
-  storeCSV: (batchItems: Array<Array<string> | Array<number>>) => Promise<boolean>;
+  storeCSV: (batchItems: BatchedDataStream) => Promise<boolean>;
   storePCA: () => Promise<boolean>;
   selectAxes: (columnXName: string, columnYName: string, columnZName: string) => Promise<boolean>;
   selectPCA: (columnXName: string, columnYName: string, columnZName: string) => Promise<boolean>;
