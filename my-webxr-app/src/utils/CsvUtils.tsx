ef79e3802c6b1ca86d4f9662c99b929f45d0fc1f
Subzeero <76539691+Subzeero@users.noreply.github.com>
2024-03-26 20:42:51 -0600

    #122: Update example usage of Rollbar
    * Add a better example of using Rollbar outside of React.

--- my-webxr-app/src/utils/CsvUtils.tsx
+++ my-webxr-app/src/utils/CsvUtils.tsx
@@ -1,9 +1,7 @@
 import { openDB } from 'idb';
 import Papa from 'papaparse';
 import React from 'react';
-import Rollbar from 'rollbar';
 import assert from './Assert';
-import { rollbarConfig } from './LoggingUtils';
 
 /**
  * Validates the existence of a database and a store within that database.
@@ -81,8 +79,6 @@ export async function parseAndHandleLocalCsv(
       if (batch.length > 0) {
         await handleParsedData(batch, dbName, storeName, i);
       }
-      const rollbar = new Rollbar(rollbarConfig);
-      rollbar.debug('Loaded CSV successfully');
       setMessage('Local CSV loaded successfully');
     },
   });
