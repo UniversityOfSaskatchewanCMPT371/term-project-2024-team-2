fbacc148d8e2747665ab12b83f01113470cc7ef7
Long Quan Nguyen <longquanab@gmail.com>
2024-03-29 20:19:22 -0600

    #239 Update getPoints in Repository to also return max values for each of 3 columns

--- my-webxr-app/src/repository/Repository.tsx
+++ my-webxr-app/src/repository/Repository.tsx
@@ -10,7 +10,7 @@ export interface Repository {
     columnXName: string,
     columnYName: string,
     columnZName: string,
-  ) => Promise<Array<DataPoint>>;
+  ) => Promise<[Array<DataPoint>, Array<number>]>;
   addColumn: (column: Column<RawColumn | StatsColumn | NumericColumn>,
     columnType: TableName) => Promise<string>;
   getCsvColumnNames: () => Promise<string[]>;
