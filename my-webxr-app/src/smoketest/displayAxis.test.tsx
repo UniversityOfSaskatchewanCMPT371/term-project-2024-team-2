4e305300b020fcab364d267c467ea5ac18b96c11
Mitchell Wagner <pim128@usask.ca>
2024-03-30 12:08:51 -0600

    Removed unnecesary tests

--- my-webxr-app/src/smoketest/displayAxis.test.tsx
+++ my-webxr-app/src/smoketest/displayAxis.test.tsx
@@ -12,20 +12,4 @@ describe('  This feature verifies the creation and manipulation of 3D axes in We
     response = await ContainText('Generated Z-Axis');
     expect(response).toBeTruthy();
   });
-  it('URL CSV is loaded successfully', async () => {
-    const response = await ContainText('URL CSV has been successfully loaded');
-    expect(response).toBeTruthy();
-  });
-  it('File System CSV is loaded successfully', async () => {
-    const response = await ContainText('File system CSV has been successfully loaded');
-    expect(response).toBeTruthy();
-  });
-  it('No file selected', async () => {
-    const response = await ContainText('No file selected');
-    expect(response).toBeTruthy();
-  });
-  it('No URL CSV entered or URL is not a csv', async () => {
-    const response = await ContainText('Url is empty or not a csv file');
-    expect(response).toBeTruthy();
-  });
 });
