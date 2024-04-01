a37998943c96e1772d6bde34b8e19870c211a027
Quinn <qpb948@usask.ca>
2024-04-01 16:31:56 -0600

    #198 - created smoke tests for displaying data in 3d space

--- my-webxr-app/src/components/GraphingDataPointMenu.tsx
+++ my-webxr-app/src/components/GraphingDataPointMenu.tsx
@@ -5,6 +5,7 @@ import { RefAttributes } from 'react';
 import { JSX } from 'react/jsx-runtime';
 import { Group } from 'three';
 import { usePointSelectionContext } from '../contexts/PointSelectionContext';
+import WriteHook from '../smoketest/TestHookWrite';
 
 /**
  * Creates a billboard to display the related information of the data point
@@ -31,16 +32,23 @@ export default function GraphingDataPointMenu(
         {/* A negative depth offset brings the object closer to the viewer */}
         <Text fontSize={0.075} color="black" depthOffset={-4}>
           {`Here are data point # ${selectedDataPoint?.id ?? '-'} properties!\n\n`}
+          {WriteHook(`| ${String(selectedDataPoint?.id)} : `)}
           {`Marker: ${selectedDataPoint?.marker ?? '-'}\n`}
+          {WriteHook(`${String(selectedDataPoint?.marker)} : `)}
           {`Color: ${selectedDataPoint?.color ?? '-'}\n`}
+          {WriteHook(`${String(selectedDataPoint?.color)} : `)}
           {`x, y, z: ${
             selectedDataPoint?.actualData
               ? `${selectedDataPoint.actualData[0]}, ${selectedDataPoint.actualData[1]}, ${selectedDataPoint.actualData[2]}`
               : '0, 0, 0'
           }\n`}
+          {WriteHook(`${String(selectedDataPoint?.actualData)} : `)}
           {`Column X: ${selectedDataPoint?.columnX ?? '-'}\n`}
+          {WriteHook(`${String(selectedDataPoint?.columnX)} : `)}
           {`Column Y: ${selectedDataPoint?.columnY ?? '-'}\n`}
+          {WriteHook(`${String(selectedDataPoint?.columnY)} : `)}
           {`Column Z: ${selectedDataPoint?.columnZ ?? '-'}`}
+          {WriteHook(`${String(selectedDataPoint?.columnZ)} |`)}
         </Text>
       </Plane>
     </Billboard>
