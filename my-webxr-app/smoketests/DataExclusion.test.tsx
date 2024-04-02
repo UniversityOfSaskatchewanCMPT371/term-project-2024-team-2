4ccbeb7f967c2e85e2a48986acf78dc1da4aaa91
Subzeero <76539691+Subzeero@users.noreply.github.com>
2024-04-02 11:01:41 -0600

    #119: Fix a typo in smoketest
    * Mixed up the number of rows for both tests. Both were still checked, but referred to the other test.

--- my-webxr-app/smoketests/DataExclusion.test.tsx
+++ my-webxr-app/smoketests/DataExclusion.test.tsx
@@ -2,12 +2,12 @@ import ContainText from './TextInFileSearch';
 
 describe('Exclude incomplete data from CSV import', () => {
   test('Loading a complete CSV file (grades.csv)', async () => {
-    const response = await ContainText('Loaded CSV with 3 rows');
+    const response = await ContainText('Loaded CSV with 31 rows');
     expect(response).toBeTruthy();
   });
 
   test('Loading an incomplete CSV file (exclusion.csv)', async () => {
-    const response = await ContainText('Loaded CSV with 31 rows');
+    const response = await ContainText('Loaded CSV with 3 rows');
     expect(response).toBeTruthy();
   });
 });
