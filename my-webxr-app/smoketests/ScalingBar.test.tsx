6d0d5ce4c8d12e0029cdc3d74d84208300a9b043
Quinn <qpb948@usask.ca>
2024-04-02 20:39:46 -0600

    #264 - smoke tests created for the scaling bar

---
+++ my-webxr-app/smoketests/ScalingBar.test.tsx
@@ -0,0 +1,32 @@
+import ContainText from './TextInFileSearch';
+
+describe('Scaling bar can change the size of the axis', () => {
+  it('Scaling bar is visible on menu', async () => {
+    const response = await ContainText('Scale bar is visible');
+    expect(response).toBeTruthy();
+  });
+  it('Scaling value is 1', async () => {
+    const response = await ContainText('1');
+    expect(response).toBeTruthy();
+  });
+  it('Scaling value is 5.5', async () => {
+    const response = await ContainText('5.5');
+    expect(response).toBeTruthy();
+  });
+  it('Scaling value is 10', async () => {
+    const response = await ContainText('10');
+    expect(response).toBeTruthy();
+  });
+  it('Length of axis changes with scaling value 1', async () => {
+    const response = await ContainText('1');
+    expect(response).toBeTruthy();
+  });
+  it('Length of axis changes with scaling value 5.5', async () => {
+    const response = await ContainText('5.5');
+    expect(response).toBeTruthy();
+  });
+  it('Length of axis changes with scaling value 10', async () => {
+    const response = await ContainText('10');
+    expect(response).toBeTruthy();
+  });
+});
